5c29a1b52047e08b24ba985f50bc5c4074119260
saileshchauhan <chauhansailesh68@gmail.com>
2021-04-30 17:58:35 +0530

    [Sailesh] Add:ListImplementation

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Implement_List();
+        }
+
+        private static void Implement_List()
+        {
+            Console.WriteLine("\nImplementing List in DS");
+            List<string> list = new List<string>();
+            //Adding object to the list
+            list.Add("Ravi");
+            list.Add("Vijay");
+            list.Add("Sailesh");
+            list.Add("Ajay");
+            list.Add("Ravi");
+            //Iterate list elements
+            foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
--- Readme.md
+++
@@ -1,1 +0,0 @@
-This is Collection Project
